83efd9b6e4d63021f077f5e166d1686435f1e21f
Satya2553 <mareedu..satyanarayana.5475@gmail.com>
2024-12-11 19:37:04 +0530

    New push

--- src/components/users/cards/Specialdealscard.js
+++ src/components/users/cards/Specialdealscard.js
@@ -3,37 +3,37 @@ import "./Specialdealscard.css";
 import { useNavigate } from "react-router-dom";
 
 const Specialdealscard = () => {
-  const [email, setEmail] = useState(""); 
-const Navigate=useNavigate()
-  const handleSubscribe = () => {
-    if (email) {
-			Navigate('/signup')
-      console.log("Entered Email:", email); 
-    } else {
-      console.log("Please enter a valid email.");
-    }
-  };
+	const [email, setEmail] = useState("");
+	const Navigate = useNavigate();
+	const handleSubscribe = () => {
+		if (email) {
+			Navigate("/signup");
+			console.log("Entered Email:", email);
+		} else {
+			console.log("Please enter a valid email.");
+		}
+	};
 
-  return (
-    <div className="Specialdealscard_container">
-      <div className="row1">
-        <h1>Join our newsletter for Special Deals & Offers</h1>
-        <p>
-          Register now to get the latest updates on promotions & coupons. Don't
-          worry, we won't spam!
-        </p>
-      </div>
-      <div className="row2">
-        <input
-          type="email"
-          placeholder="Enter your Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)} 
-        />
-        <button onClick={handleSubscribe}>Subscribe</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="Specialdealscard_container">
+			<div className="row1">
+				<h1>Join our newsletter for Special Deals & Offers</h1>
+				<p>
+					Register now to get the latest updates on promotions & coupons.{" "}
+					<br></br>Don't worry, we won't spam!
+				</p>
+			</div>
+			<div className="row2">
+				<input
+					type="email"
+					placeholder="Enter your Email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<button onClick={handleSubscribe}>Subscribe</button>
+			</div>
+		</div>
+	);
 };
 
 export default Specialdealscard;
