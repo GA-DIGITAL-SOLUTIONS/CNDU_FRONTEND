0b64ea6a99e2a28786a277c4a1c5411b936b0393
AmeerBashap <ameerpotuganti2@gmail.com>
2025-06-19 10:13:26 +0530

    added ashadam banner

--- src/components/users/Body/Section1/Section1.js
+++ src/components/users/Body/Section1/Section1.js
@@ -1,29 +1,66 @@
 import React from "react";
 import "./Section1.css";
 import { useNavigate } from "react-router-dom";
+import { Carousel, message } from "antd";
+// import cnduashadamsale from '../bannerimages/cnduashadamsale.jpg'
+import ashadam2 from '../bannerimages/ashadam2.jpg'
+import cnduashadamsale from "../bannerimages/cnduashadamsale.jpg"
+import cnduashadamsale2 from '../bannerimages/cnduashadamsale2.jpg'
 
 const Section1 = () => {
-	const navigate = useNavigate();
-	return (
-		<div className="section1">
-			<img src="/HomePageBanner.jpg" alt="Banner" className="section1-image" />
-			<div className="section-text">
-				<div className="heading-text">
-					Getting the best and <br /> latest style has never <br />
-					<span>been easier!</span>
-				</div>
-				<div className="subtext-section">
-					<span>CNDU FABRICS</span> is a platform that helps to make fashion
-					accessible to all. It brings fashion to your doorstep!
-				</div>
-				<div className="section-button">
-					<button onClick={() => navigate("/CNDUCollections")} style={{cursor:"pointer"}}>
-						Shop collections
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  const navigate = useNavigate();
+
+  const handleNavigate =()=>{
+    navigate("/offers");
+  }
+  
+  return (
+    <>
+      <div className="section1" onClick={handleNavigate}>
+        <img
+          // src="/HomePageBanner.jpg"
+          src={cnduashadamsale2}
+          alt="Banner"
+          className="section1-image"
+        />
+        {/* <div className="section-text">
+          <div className="heading-text">
+            Getting the best and <br /> latest style has never <br />
+            <span>been easier!</span>
+          </div>
+          <div className="subtext-section">
+            <span>CNDU FABRICS</span> is a platform that helps to make fashion
+            accessible to all. It brings fashion to your doorstep!
+          </div>
+          <div className="section-button">
+            <button
+              onClick={() => navigate("/CNDUCollections")}
+              style={{ cursor: "pointer" }}
+            >
+              Shop collections
+            </button>
+          </div>
+        </div> */}
+      </div>
+			{/*  */}
+      {/* <div style={{ width: "100%"}}>
+        <Carousel autoplay={true} effect="scrollx" infinite>
+          <div className="home-banner">
+            <img src={ashadam2}alt="SHEagle Trading" />
+            <div className="bannercontent">
+              <span>NEW IN</span>STUDIO COLLECTION
+            </div>
+          </div>
+        </Carousel>
+      </div> */}
+    </>
+  );
 };
 
 export default Section1;
+
+
+
+
+
+
