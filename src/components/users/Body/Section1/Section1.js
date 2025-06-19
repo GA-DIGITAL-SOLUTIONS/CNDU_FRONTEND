3f388fcc21840501e1df26c18c7668da53e97c4b
AmeerBashap <ameerpotuganti2@gmail.com>
2025-06-19 11:15:16 +0530

    added btn in banner

--- src/components/users/Body/Section1/Section1.js
+++ src/components/users/Body/Section1/Section1.js
@@ -5,7 +5,7 @@ import { Carousel, message } from "antd";
 // import cnduashadamsale from '../bannerimages/cnduashadamsale.jpg'
 import ashadam2 from '../bannerimages/ashadam2.jpg'
 import cnduashadamsale from "../bannerimages/cnduashadamsale.jpg"
-import cnduashadamsale2 from '../bannerimages/cnduashadamsale2.jpg'
+import cnduashadamsale3 from '../bannerimages/cnduashadamsale3.jpg'
 
 const Section1 = () => {
   const navigate = useNavigate();
@@ -13,34 +13,34 @@ const Section1 = () => {
   const handleNavigate =()=>{
     navigate("/offers");
   }
-  
+
   return (
     <>
       <div className="section1" onClick={handleNavigate}>
         <img
           // src="/HomePageBanner.jpg"
-          src={cnduashadamsale2}
+          src={cnduashadamsale3}
           alt="Banner"
           className="section1-image"
         />
-        {/* <div className="section-text">
-          <div className="heading-text">
+        <div className="section-text">
+          {/* <div className="heading-text">
             Getting the best and <br /> latest style has never <br />
             <span>been easier!</span>
           </div>
           <div className="subtext-section">
             <span>CNDU FABRICS</span> is a platform that helps to make fashion
             accessible to all. It brings fashion to your doorstep!
-          </div>
+          </div> */}
           <div className="section-button">
             <button
-              onClick={() => navigate("/CNDUCollections")}
+              onClick={() => navigate("/offers")}//CNDUCollections
               style={{ cursor: "pointer" }}
             >
-              Shop collections
+              Shop now
             </button>
           </div>
-        </div> */}
+        </div>
       </div>
 			{/*  */}
       {/* <div style={{ width: "100%"}}>
