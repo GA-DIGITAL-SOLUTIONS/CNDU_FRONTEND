3ac3f61e918f6b2c6fe7bb8fed5527bce9800e77
Satya2553 <mareedu.satyanarayana.5475@gmail.com>
2025-06-20 13:01:42 +0530

    Paginated Views

--- src/components/users/Fabrics/Fabricspage.js
+++ src/components/users/Fabrics/Fabricspage.js
@@ -330,7 +330,7 @@ const Fabricspage = () => {
                             <Link to={`/fabrics/${product.id}`}>
                               <img
                                 alt={product.name}
-                                src={`${apiurl}${firstColorImage}`}
+                                src={`${firstColorImage}`}
                                 style={{
                                   cursor: "pointer",
                                   width: "100%",
